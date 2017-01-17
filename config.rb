4fe49a6b32dbd9b6b2286a3059212779afc37d0d
George Lu <glu@canvas.is>
2017-01-16 20:31:21 -0500

    initial setup

---
+++ config.rb
@@ -0,0 +1,40 @@
+require 'slim'
+
+activate :external_pipeline,
+         name: :webpack,
+         command: build? ? "npm run build" : "npm start",
+         source: ".tmp/dist",
+         latency: 1
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+end
